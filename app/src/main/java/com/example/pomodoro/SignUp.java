b007a8cea6b4d17bd2edd0e4852634d8f1f15eb1
Tian Hang <ezioht5312@gmail.com>
2020-11-06 11:40:24 +0800

    Edit style of Login, add on comments of login

--- app/src/main/java/com/example/pomodoro/SignUp.java
+++ app/src/main/java/com/example/pomodoro/SignUp.java
@@ -19,13 +19,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUp extends AppCompatActivity {
+
+    //Initial the button
     private Button BacktoSign;
     private Button SignUp;
 
+    //Initial the Edit test
     private EditText MailCurrent;
     private EditText Password1;
     private EditText Password2;
 
+    //Initial Firebase authority
     private FirebaseAuth mAuth;
 
 
@@ -35,15 +39,20 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        //Binding the button
         this.BacktoSign = (Button)findViewById(R.id.back);
         this.SignUp = (Button)findViewById(R.id.signup);
 
+        //Binding the user input
         this.MailCurrent = (EditText)findViewById(R.id.emailSignUp);
         this.Password1 = (EditText)findViewById(R.id.password1);
         this.Password2 = (EditText)findViewById(R.id.password2);
 
+        //Binding the FirebaseAuth instance
         this.mAuth = FirebaseAuth.getInstance();
 
+        //Click on back to sign
+        //go back to sign in pages
         this.BacktoSign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -51,36 +60,41 @@ public class SignUp extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //Click on sign up button
         this.SignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //check whether current email is empty
                 if (MailCurrent == null) {
                     Toast.makeText(getApplicationContext(), "Please enter Your email !", Toast.LENGTH_SHORT).show();
                 } else {
+                    //check whether two passwords are the same
                     if (!Password1.getText().toString().equals(Password2.getText().toString())) {
                         Toast.makeText(getApplicationContext(), "Different Password !", Toast.LENGTH_SHORT).show();
                     } else {
+                        //Check the length of password
                         String emailInput = MailCurrent.getText().toString();
                         String passwordInput = Password1.getText().toString();
                         if (passwordInput.length() <= 5) {
                             Toast.makeText(SignUp.this, "Password should not be empty or less than 6 characters", Toast.LENGTH_SHORT).show();
                         } else {
-                            Toast.makeText(SignUp.this, "Creating... It might take a long time", Toast.LENGTH_SHORT).show();
+                            //Create new user
+                            Toast.makeText(SignUp.this, "Creating... ", Toast.LENGTH_SHORT).show();
+
+                            //Call function in Firebase
                             mAuth.createUserWithEmailAndPassword(emailInput, passwordInput)
                                     .addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
                                         @Override
                                         public void onComplete(@NonNull Task<AuthResult> task) {
                                             if (task.isSuccessful()) {
-                                                // Sign in success, update UI with the signed-in user's information
-                                                //FirebaseUser user = mAuth.getCurrentUser();
-                                                Toast.makeText(SignUp.this, "Successfully Create User! Generating Data...",
-                                                        Toast.LENGTH_SHORT).show();
+                                                //Sign in successfully
+                                                Toast.makeText(SignUp.this, "Successfully Create User! Generating Data...", Toast.LENGTH_SHORT).show();
                                                 createData(mAuth.getCurrentUser());
+                                                //Sign out first
                                                 mAuth.signOut();
                                             } else {
-                                                // If sign in fails, display a message to the user.
-                                                Toast.makeText(SignUp.this, "Create User failed.",
-                                                        Toast.LENGTH_SHORT).show();
+                                                Toast.makeText(SignUp.this, "Create User failed.", Toast.LENGTH_SHORT).show();
                                             }
                                         }
                                     });
@@ -92,6 +106,9 @@ public class SignUp extends AppCompatActivity {
 
     }
     private void createData(FirebaseUser currentUser){
+        //Initial setting of the config values
+        //All the integers are set to 0
+
         String UID = currentUser.getUid();
         String Nickname = "Pomodoro";
         String Location = "Not Check In";
@@ -104,6 +121,7 @@ public class SignUp extends AppCompatActivity {
         long Latitude = 0;
         long Longitude = 0;
 
+        //Initial all the parameters which will be used.
         DatabaseReference database = FirebaseDatabase.getInstance().getReference();
         database.child("User").child(UID).setValue(1);
         database.child("User").child(UID).child("LongestChallenge").setValue(LongestStreakData);
@@ -120,7 +138,10 @@ public class SignUp extends AppCompatActivity {
 
 
 
-        Toast.makeText(SignUp.this, " Generating Data Successfully",
-                Toast.LENGTH_SHORT).show();
+        Toast.makeText(SignUp.this, "Sign up Successfully, Back to Login...", Toast.LENGTH_SHORT).show();
+
+        //Return to login page if sign up is done
+        Intent intent=new Intent(SignUp.this,LoginActivity.class);
+        startActivity(intent);
     }
 }
